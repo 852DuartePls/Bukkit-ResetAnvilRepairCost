659e5c0904a144c280b2a567616f43368be8ed31
852DuartePls <duartebenitez_001@hotmail.com>
2024-09-03 23:07:52 -0400

    First commit

---
+++ build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    `java-library`
+}
+
+group = "me.duart"
+version = "0.0.1"
+description = "ResetAnvilRepairCost"
+
+repositories {
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    maven("https://oss.sonatype.org/content/groups/public/")
+}
+
+dependencies {
+    compileOnly("org.spigotmc:spigot-api:1.20-R0.1-SNAPSHOT")
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+}
+
+tasks {
+
+    compileJava {
+        options.encoding = Charsets.UTF_8.name()
+        options.release.set(17)
+    }
+    javadoc {
+        options.encoding = Charsets.UTF_8.name()
+    }
+    processResources {
+        filteringCharset = Charsets.UTF_8.name()
+        val props = mapOf(
+            "name" to project.name,
+            "version" to project.version,
+            "description" to project.description,
+            "apiVersion" to "1.20"
+        )
+        inputs.properties(props)
+        filesMatching("plugin.yml") {
+            expand(props)
+        }
+    }
+}
