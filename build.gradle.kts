eeab6d68d28bfc3aa90ee63afc86c2ea5acac920
852DuartePls <duartebenitez_001@hotmail.com>
2024-09-07 19:26:48 -0400

    Cache configuration and some small rewrites.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,15 +3,15 @@ plugins {
 }
 
 group = "me.duart"
-version = "0.0.2"
+version = "0.0.3"
 description = "Reset Anvil Repair Cost"
+
 repositories {
     maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
     maven("https://oss.sonatype.org/content/groups/public/")
 }
 
 dependencies {
-
     compileOnly("org.spigotmc:spigot-api:1.20-R0.1-SNAPSHOT")
 }
 
@@ -20,25 +20,8 @@ java {
 }
 
 tasks {
-
     compileJava {
         options.encoding = Charsets.UTF_8.name()
         options.release.set(17)
-    }
-    javadoc {
-        options.encoding = Charsets.UTF_8.name()
-    }
-    processResources {
-        filteringCharset = Charsets.UTF_8.name()
-        val props = mapOf(
-            "name" to project.name,
-            "version" to project.version,
-            "description" to project.description,
-            "apiVersion" to "1.20"
-        )
-        inputs.properties(props)
-        filesMatching("plugin.yml") {
-            expand(props)
-        }
     }
 }
--- src/main/java/me/duart/resetAnvilRepairCost/ResetAnvilRepairCost.java
+++ src/main/java/me/duart/resetAnvilRepairCost/ResetAnvilRepairCost.java
@@ -16,27 +16,26 @@ import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.Repairable;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public final class ResetAnvilRepairCost extends JavaPlugin implements Listener {
     ConsoleCommandSender console;
     private final String PluginName = this.getName();
 
-    private int MAX_REPAIR_COST;
-    private int ADJUSTED_COST;
+    private final Map<String, Integer> configCache = new HashMap<>();
 
     @Override
     public void onEnable() {
         saveDefaultConfig();
-        FileConfiguration config = getConfig();
-
-        this.MAX_REPAIR_COST = config.getInt("value.max_repair_cost", 35);
-        this.ADJUSTED_COST = config.getInt("value.adjusted_cost", 20);
-
+        loadConfigValues();
         console = getServer().getConsoleSender();
-        console.sendMessage(ChatColor.YELLOW + PluginName + " || enabled!");
-        console.sendMessage(ChatColor.YELLOW + PluginName + " || Max Repair Cost: " + ChatColor.GREEN + MAX_REPAIR_COST);
-        console.sendMessage(ChatColor.YELLOW + PluginName + " || Adjusted Cost: " + ChatColor.GREEN + ADJUSTED_COST);
+        console.sendMessage(ChatColor.YELLOW + PluginName + " || " + "Enabled!");
+        console.sendMessage(ChatColor.YELLOW + PluginName + " || "
+                +"Max Repair Cost: " + ChatColor.GREEN + configCache.get("max_repair_cost"));
+        console.sendMessage(ChatColor.YELLOW + PluginName + " || "
+                +"Adjusted Cost: " + ChatColor.GREEN + configCache.get("adjusted_cost"));
         Objects.requireNonNull(getCommand("anvilresetcost")).setExecutor(new ReloadCommand(this));
         Objects.requireNonNull(getCommand("anvilresetcost")).setTabCompleter(new ReloadCommand(this));
         getServer().getPluginManager().registerEvents(this, this);
@@ -45,27 +44,34 @@ public final class ResetAnvilRepairCost extends JavaPlugin implements Listener {
     // Reloads the plugin configuration.
     public void onReload() {
         reloadConfig();
-        FileConfiguration config = getConfig();
-
-        this.MAX_REPAIR_COST = config.getInt("value.max_repair_cost", 35);
-        this.ADJUSTED_COST = config.getInt("value.adjusted_cost", 20);
-        console.sendMessage(ChatColor.YELLOW + PluginName + "|| Modified Max Repair Cost set to: " + ChatColor.GREEN + MAX_REPAIR_COST);
-        console.sendMessage(ChatColor.YELLOW + PluginName + "|| Modified Adjusted Cost set to: " + ChatColor.GREEN + ADJUSTED_COST);
-
+        loadConfigValues();
+        console.sendMessage(ChatColor.YELLOW + PluginName + " || "
+                +"Modified Max Repair Cost set to: " + ChatColor.GREEN + configCache.get("max_repair_cost"));
+        console.sendMessage(ChatColor.YELLOW + PluginName + " || "
+                +"Modified Adjusted Cost set to: " + ChatColor.GREEN + configCache.get("adjusted_cost"));
     }
 
-    // Everytime an item surpasses the MAX_REPAIR_COST, it will be set to the ADJUSTED_COST.
+    private void loadConfigValues() {
+        FileConfiguration config = getConfig();
+        configCache.put("max_repair_cost", config.getInt("value.max_repair_cost", 35));
+        configCache.put("adjusted_cost", config.getInt("value.adjusted_cost", 20));
+    }
+
+    // Every time an item surpasses the MAX_REPAIR_COST, it will be set to the ADJUSTED_COST.
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onPrepareAnvil(PrepareAnvilEvent event) {
         ItemStack item = event.getResult();
         if (item == null || item.getItemMeta() == null) return;
-        if (noPermission((Player) event.getView().getPlayer())) return;
+        if (hasPlayerPermission((Player) event.getView().getPlayer())) return;
 
         ItemMeta meta = item.getItemMeta();
         if (meta instanceof Repairable repairable) {
             int repairCost = repairable.getRepairCost();
-            if (repairCost >= MAX_REPAIR_COST) {
-                repairable.setRepairCost(ADJUSTED_COST);
+            int maxRepairCost = configCache.get("max_repair_cost");
+            int adjustedCost = configCache.get("adjusted_cost");
+
+            if (repairCost >= maxRepairCost) {
+                repairable.setRepairCost(adjustedCost);
                 item.setItemMeta(meta);
                 event.setResult(item);
             }
@@ -73,20 +79,23 @@ public final class ResetAnvilRepairCost extends JavaPlugin implements Listener {
     }
 
     // If an item is already with a repair cost that is >= MAX_REPAIR_COST,
-    // this will be set to the ADJUSTED_COST, when clicking on the item again.
+    // this will be set to the ADJUSTED_COST when clicking on the item again.
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onInventoryClick(InventoryClickEvent event) {
         if (event.getInventory().getType() != InventoryType.ANVIL) return;
         if (event.getRawSlot() != 0) return;
         ItemStack item = event.getCurrentItem();
         if (item == null || item.getItemMeta() == null) return;
-        if (noPermission((Player) event.getWhoClicked())) return;
+        if (hasPlayerPermission((Player) event.getWhoClicked())) return;
 
         ItemMeta meta = item.getItemMeta();
         if (meta instanceof Repairable repairable) {
             int repairCost = repairable.getRepairCost();
-            if (repairCost >= MAX_REPAIR_COST) {
-                repairable.setRepairCost(ADJUSTED_COST);
+            int maxRepairCost = configCache.get("max_repair_cost");
+            int adjustedCost = configCache.get("adjusted_cost");
+
+            if (repairCost >= maxRepairCost) {
+                repairable.setRepairCost(adjustedCost);
                 item.setItemMeta(meta);
                 event.getInventory().setItem(event.getSlot(), item);
             }
@@ -94,7 +103,7 @@ public final class ResetAnvilRepairCost extends JavaPlugin implements Listener {
     }
 
     // Permission check.
-    private boolean noPermission(Player player) {
+    private boolean hasPlayerPermission(Player player) {
         return !player.hasPermission("resetrepaircost.user");
     }
 }
